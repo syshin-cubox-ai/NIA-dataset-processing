1580805036bdc41a462614702e3e6d87f077ba3d
Seokyong Shin <syshin@cubox.ai>
2022-09-28 17:49:30 +0900

    확장자 대문자 이슈 고침

--- extract_img_json.py
+++ extract_img_json.py
@@ -7,11 +7,10 @@ indoor_label_path = 'D:/data/NIA_2022_1/122-1.다중객체3차원표현데이터
 outdoor_img_path = 'D:/data/NIA_2022_1/122-2.다중객체3차원표현데이터(실외)/01.원천데이터'
 outdoor_label_path = 'D:/data/NIA_2022_1/122-2.다중객체3차원표현데이터(실외)/02.라벨링데이터(2D)'
 
-indoor_img = [i for i in glob.glob(os.path.join(indoor_img_path, '**'), recursive=True) if os.path.splitext(i)[1] == '.JPG']
+indoor_img = [i.replace('.JPG', '.jpg') for i in glob.glob(os.path.join(indoor_img_path, '**'), recursive=True) if os.path.splitext(i)[1] == '.JPG']
 indoor_label = [i for i in glob.glob(os.path.join(indoor_label_path, '**'), recursive=True) if os.path.splitext(i)[1] == '.json']
-outdoor_img = [i for i in glob.glob(os.path.join(outdoor_img_path, '**'), recursive=True) if os.path.splitext(i)[1] == '.JPG']
+outdoor_img = [i.replace('.JPG', '.jpg') for i in glob.glob(os.path.join(outdoor_img_path, '**'), recursive=True) if os.path.splitext(i)[1] == '.JPG']
 outdoor_label = [i for i in glob.glob(os.path.join(outdoor_label_path, '**'), recursive=True) if os.path.splitext(i)[1] == '.json']
-
 
 indoor_img_path_temp = os.path.join(indoor_img_path, 'temp')
 outdoor_img_path_temp = os.path.join(outdoor_img_path, 'temp')
@@ -29,11 +28,11 @@ os.makedirs(outdoor_output_path, exist_ok=True)
 for label in indoor_label:
     shutil.copy(label, indoor_output_path)
     filename = os.path.splitext(os.path.basename(label))[0]
-    shutil.move(os.path.join(indoor_img_path_temp, filename + '.JPG'), indoor_output_path)
+    shutil.move(os.path.join(indoor_img_path_temp, filename + '.jpg'), indoor_output_path)
 for label in outdoor_label:
     try:
         filename = os.path.splitext(os.path.basename(label))[0]
-        shutil.move(os.path.join(outdoor_img_path_temp, filename + '.JPG'), outdoor_output_path)
+        shutil.move(os.path.join(outdoor_img_path_temp, filename + '.jpg'), outdoor_output_path)
         shutil.copy(label, outdoor_output_path)
     except:
         pass

55f8af4dcdf4b4f7275dc3caab27271d8b9bf442
Seokyong Shin <syshin@cubox.ai>
2022-09-29 15:56:41 +0900

    Fix split issue

--- split_trainval.py
+++ split_trainval.py
@@ -4,7 +4,7 @@ import json
 import os
 
 TYPE = 'indoor'
-input_json_path = f'D:/data/nia/{TYPE}_trainval.json'
+input_json_path = f'D:/data/nia/annotations/{TYPE}_trainval.json'
 
 with open(input_json_path, 'r', encoding='utf-8') as f:
     coco_format_json = json.load(f)
@@ -12,8 +12,8 @@ with open(input_json_path, 'r', encoding='utf-8') as f:
 template = collections.OrderedDict(
     {
         'info': coco_format_json['info'],
-        'images': None,
-        'annotations': None,
+        'images': [],
+        'annotations': [],
         'categories': coco_format_json['categories']
     }
 )
@@ -21,10 +21,19 @@ train_label = copy.deepcopy(template)
 val_label = copy.deepcopy(template)
 del template
 
-val_label['images'] = coco_format_json['images'][:10]
-val_label['annotations'] = coco_format_json['annotations'][:30]
-train_label['images'] = coco_format_json['images'][10:]
-train_label['annotations'] = coco_format_json['annotations'][30:]
+val_img_id = []
+for i in range(len(coco_format_json['images'])):
+    if i % 10 == 0:
+        val_img_id.append(i)
+        val_label['images'].append(coco_format_json['images'][i])
+    else:
+        train_label['images'].append(coco_format_json['images'][i])
+
+for ann in coco_format_json['annotations']:
+    if ann['image_id'] in val_img_id:
+        val_label['annotations'].append(ann)
+    else:
+        train_label['annotations'].append(ann)
 
 with open(os.path.join(os.path.dirname(input_json_path), f'{TYPE}_train.json'), 'w', encoding='utf-8') as f:
     json.dump(train_label, f)
